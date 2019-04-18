9f1d84a4a00d8f2b8e3a94a3dd12cefcc570dde6
htappa <htappa@gmail.com>
2019-04-18 15:28:44 -0400

    added CNN and hyper-parameters

--- Code/fruits_cnn.py
+++ Code/fruits_cnn.py
@@ -4,6 +4,8 @@
 
 import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.autograd import Variable
 import numpy as np
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -16,13 +18,38 @@ import numpy as np
 # HYPER-PARAMETERS
 # ----------------------------------------------------------------------------------------------------------------------
 
-
+num_epochs = 10
+batch_size = 100
+alpha = 0.0001
 
 # ----------------------------------------------------------------------------------------------------------------------
-# CONVOLUTIONAL NEURAL NETWORK MODEL
+# CONVOLUTIONAL NEURAL NETWORK (CNN) MODEL
 # ----------------------------------------------------------------------------------------------------------------------
 
+# 2 layer network
+# batch shape for input x is (3, 100, 100)
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            # input channels = 3, output channels = 50
+            nn.Conv2d(3, 50, kernel_size=10, padding=2),
+            nn.BatchNorm2d(50),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(50, 100, kernel_size=10, padding=2),
+            nn.BatchNorm2d(100),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(500, 33)
 
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 # ----------------------------------------------------------------------------------------------------------------------
 # LOSS & OPTIMIZER

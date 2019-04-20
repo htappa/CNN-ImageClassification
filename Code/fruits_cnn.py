c57688b8f36f0d0c2c9a7baadb0fbccfde0a8d90
ryankinsey1 <kinsey.8.rk@gmail.com>
2019-04-19 21:21:17 -0400

    Added Load Dataset

--- Code/fruits_cnn.py
+++ Code/fruits_cnn.py
@@ -52,27 +52,27 @@ alpha = 0.0001
 # ----------------------------------------------------------------------------------------------------------------------
 
 # 2 layer network
-# class CNN(nn.Module):
-#     def __init__(self):
-#         super(CNN, self).__init__()
-#         self.layer1 = nn.Sequential(
-#             nn.Conv2d(in_channels=3, out_channels=50, kernel_size=10, padding=2),
-#             nn.BatchNorm2d(num_features=50),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2))
-#         self.layer2 = nn.Sequential(
-#             nn.Conv2d(in_channels=50, out_channels=100, kernel_size=10, padding=2),
-#             nn.BatchNorm2d(num_features=100),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2))
-#         self.fc = nn.Linear(in_features=100*48*48, out_features=33) #in_features = [(inputsize + 2*pad - kernelsize)/stride] + 1﻿
-#
-#     def forward(self, x):
-#         out = self.layer1(x)
-#         out = self.layer2(out)
-#         out = out.view(out.size(0), -1)
-#         out = self.fc(out)
-#         return out
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=50, kernel_size=10, padding=2),
+            nn.BatchNorm2d(num_features=50),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=50, out_channels=100, kernel_size=10, padding=2),
+            nn.BatchNorm2d(num_features=100),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(in_features=100*48*48, out_features=33) #in_features = [(inputsize + 2*pad - kernelsize)/stride] + 1﻿
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 # ----------------------------------------------------------------------------------------------------------------------
 # LOSS & OPTIMIZER

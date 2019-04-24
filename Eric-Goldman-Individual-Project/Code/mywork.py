ca3b569f2dd6c056a090a295fa640a70220c2c85
EricGoldman <eric.goldman16@gmail.com>
2019-04-23 20:49:23 -0400

    updated files

--- Eric-Goldman-Individual-Project/Code/mywork.py
+++ Eric-Goldman-Individual-Project/Code/mywork.py
@@ -8,7 +8,7 @@
 train_dict = "./fruits_data_set/Training"
 test_dict = "./fruits_data_set/Testing"
 
-#Updated the methodlogy when we decided to use ImageFolder to bring in the pictures
+#Updated the methodology when we decided to use ImageFolder to bring in the pictures
 train_data = datasets.ImageFolder(train_dict, transform= transform)
 test_data = datasets.ImageFolder(test_dict, transform= transform)
 
@@ -40,7 +40,19 @@ plt.show()
 num_epochs = 6
 alpha = 0.0001
 num_classes = 33
+# define cnn and set to run on GPU
+cnn = CNN()
+cnn.cuda()
+print()
+print('CNN Architecture: ' , (cnn))
+print()
 
+for images, labels in test_loader:
+    images = Variable(images).cuda()
+    outputs = cnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
 # Worked on saving the matrix to a text file. We ultimately used pandas but test numpy as well.
 # print test accuracy
 print()
@@ -49,3 +61,7 @@ print('Test Accuracy of model on the 5195 test images: %.2f %%' % (100 * correct
 # turn confusion matrix into csv
 confusion_matrix = pd.DataFrame(confusion_matrix.numpy())
 confusion_matrix.to_csv('fruits_cnn_3layer_confmat.csv')
+
+# print run time
+print()
+print('fruits_cnn_subset1 run time: %.2f seconds' % (time.time() - start_time))
